72a4501f044f15d7042eda3c1223a0c4a92b96ee
xyqlx <saint.diana@outlook.com>
2022-03-24 07:48:49 -0400

    add login function

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,17 +1,14 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { HttpClient } from "@angular/common/http";
 import { environment } from './../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ChocoService {
+export class AuthService {
   private base = environment.apiUrl;
   constructor(private http: HttpClient) { }
-  get(route: string){
-    return this.http.get(this.base + route);
-  }
-  getGpu(){
-    return this.get('gpu');
+  login(username: string, password: string){
+    return this.http.post(this.base + 'auth/login', {username, password});
   }
 }
